3c674c83c56781ff5e434ebd755f5f0c29dfecb2
alirezasamadias <alireza.samadi.as.as@gmail.com>
2024-02-11 19:10:59 +0330

    edit-about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -26,7 +26,7 @@ function About() {
                     <img src={Profile} alt={Profile} className='profile'/>
                     <figcaption className='caption'>
                         <h2 className='title-md'>Hello, I am Alireza, I am 22 years old and it has been a year since I got acquainted with the world of programming.</h2>
-                        <p>I started training from the Laitec training group affiliated with Sharif University of Technology and after that I tried to learn more with online courses.<br/>I am well-versed in Html5 and Css3, and I also know JavaScript at an intermediate level, I am familiar with sass, bootstrap and Jquery. I worked with git and github and the portfolio is fully responsive, there is also a small project related to rest api skills in the portfolio.<br/>I recently learned the react course and put some work samples in my resume.</p>
+                        <p>I started training from the Laitec training group affiliated with Sharif University of Technology and after that I tried to learn more with online courses.<br/>I am well-versed in Html5 and Css3, and I also know JavaScript at an intermediate level, I am familiar with Sass, Bootstrap and Jquery. I worked with git and github and the portfolio is fully responsive, there is also a small project related to rest api skills in the portfolio.<br/>I recently learned the React and Vue course and put some work samples in my resume.</p>
                     </figcaption>
                 </figure>
             </article>
