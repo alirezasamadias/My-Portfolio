214a0e3626ad1b6943c606267f4d04cc8e1d1393
alirezasamadias <alireza.samadi.as.as@gmail.com>
2024-02-18 18:12:02 +0330

    add-tooltip

--- src/Parts/Footer/Footer.jsx
+++ src/Parts/Footer/Footer.jsx
@@ -6,14 +6,22 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <footer className="footer">
+        <footer className='footer'>
             <div className='container'>
                 <p>&copy; 2024 alireza samadi</p>
             
-                <nav>
-                    <SocialMedia url='https://wa.me/+989104657178' icon='icon-whatsapp' />
-                    <SocialMedia url='https://github.com/alirezasamadias/' icon='icon-github' />
-                    <SocialMedia url='https://t.me/Alirezasa_as/' icon='icon-telegram' />
+                <nav className='footer-navbar'>
+                    <div className='tooltip' data-tooltip='whatsapp'>
+                        <SocialMedia url='https://wa.me/+989104657178' icon='icon-whatsapp' />
+                    </div>
+
+                    <div className='tooltip' data-tooltip='github'>
+                        <SocialMedia url='https://github.com/alirezasamadias/' icon='icon-github' />
+                    </div>
+
+                    <div className='tooltip' data-tooltip='telegram'>
+                        <SocialMedia url='https://t.me/Alirezasa_as/' icon='icon-telegram' />
+                    </div>
                 </nav>
             </div>
 
